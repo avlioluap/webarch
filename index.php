1888309f3a2b317deb5329170b13b0894c242c00
avlioluap <spooka8@gmail.com>
2014-03-14 21:32:45 +0000

    css changes
    css and img changes

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 		<link rel='stylesheet' id='style-css'  href='css/layout.css' type='text/css' media='all' />	
 		
 		<script src="http://code.jquery.com/jquery-git.js" type="text/javascript"></script>
+		<script src="js/layout_beahvior.js" type="text/javascript"></script>
 	
 
 
@@ -26,7 +27,15 @@
 	<div id="wrapper_content">
 
 		<div id="header">
-			<div id="logo">logo</div>
+			<!-- logotipo -->
+			<div id="logo">
+				<span class="logotipo"></span>
+				<ul>
+					<li><a href="#"><img src="img/home_ic.png" /></a></li>
+					<li><a href="#"><img src="img/mail_ic.png" /></a></li>
+				</ul>
+			</div>
+			
 			<div id="header_menu">
 				<ul id="header_menu_left">
 					<li>links</li>
@@ -54,77 +63,7 @@
 	1
 	</div>
 
-			
-			
-
-    
-	<script type="text/javascript">	
-	function mySetupFunction() {
-		
-		var remaining_width = parseInt($(window).width());
-		var one = $('#wrapper_content').width();
-		var two = $('#menurigth').width();
-		var three = $('#menuleft').width();	
-		
-		$('#menurigth').hide();
-		$('#menuleft').show();
-		if (window.innerWidth < 1045)
-		{
-			$('#logo').hide();
-		}
-		if (window.innerWidth > 1045)
-		{
-			$('#logo').show();
-		}		
-		$('a#hidechat').attr('id', 'showchat')			
-		$('#wrapper_content').css('width', remaining_width);
-		$('#content').css('width', remaining_width - three);
-		
-	}
-		
-	$(document).ready(function () {	
-		
-		$(document).on('click', 'a#showchat', function () {
-		
-		var remaining_width = parseInt($(window).width());
-		var one = $('#wrapper_content').width();
-		var two = $('#menurigth').width();
-		var three = $('#menuleft').width();	
-		
-			$('#menurigth').animate({ width: 'toggle' });
-			$('#menuleft').hide();
-			$('#logo').hide();
-			
-			$('#wrapper_content').css('width', remaining_width - two);
-			$('#content').css('width', remaining_width - two);
-			$('a#showchat').attr('id', 'hidechat');
-			
-		});
-
-		$(document).on('click', 'a#hidechat', function () {
-		
-		var remaining_width = parseInt($(window).width());
-		var one = $('#wrapper_content').width();
-		var two = $('#menurigth').width();
-		var three = $('#menuleft').width();			
-		
-			$('#menurigth').animate({ width: 'toggle' });
-			$('#menuleft').animate({ width: 'toggle' });
-			if (window.innerWidth > 1045)
-			{
-				$('#logo').animate({ width: 'toggle' });
-			}
-			
-			$('#wrapper_content').css('width', remaining_width);
-			$('#content').css('width', remaining_width - three);
-			$('a#hidechat').attr('id', 'showchat');
-			
-		});
-		mySetupFunction();
-	});
-
-	$(window).resize(function () { mySetupFunction(); });		
-    </script>
+					
 	
 </body>
 	
---
+++ js/layout_beahvior.js
@@ -0,0 +1,65 @@
+	function mySetupFunction() {
+		
+		var remaining_width = parseInt($(window).width());
+		var one = $('#wrapper_content').width();
+		var two = $('#menurigth').width();
+		var three = $('#menuleft').width();	
+		
+		$('#menurigth').hide();
+		$('#menuleft').show();
+		if (window.innerWidth < 1045)
+		{
+			$('#logo').hide();
+		}
+		if (window.innerWidth > 1045)
+		{
+			$('#logo').show();
+		}		
+		$('a#hidechat').attr('id', 'showchat')			
+		$('#wrapper_content').css('width', remaining_width);
+		$('#content').css('width', remaining_width - three);
+		
+	}
+		
+	$(document).ready(function () {	
+		
+		$(document).on('click', 'a#showchat', function () {
+		
+		var remaining_width = parseInt($(window).width());
+		var one = $('#wrapper_content').width();
+		var two = $('#menurigth').width();
+		var three = $('#menuleft').width();	
+		
+			$('#menurigth').animate({ width: 'toggle' });
+			$('#menuleft').hide();
+			$('#logo').hide();
+			
+			$('#wrapper_content').css('width', remaining_width - two);
+			$('#content').css('width', remaining_width - two);
+			$('a#showchat').attr('id', 'hidechat');
+			
+		});
+
+		$(document).on('click', 'a#hidechat', function () {
+		
+		var remaining_width = parseInt($(window).width());
+		var one = $('#wrapper_content').width();
+		var two = $('#menurigth').width();
+		var three = $('#menuleft').width();			
+		
+			$('#menurigth').animate({ width: 'toggle' });
+			$('#menuleft').animate({ width: 'toggle' });
+			if (window.innerWidth > 1045)
+			{
+				$('#logo').animate({ width: 'toggle' });
+			}
+			
+			$('#wrapper_content').css('width', remaining_width);
+			$('#content').css('width', remaining_width - three);
+			$('a#hidechat').attr('id', 'showchat');
+			
+		});
+		mySetupFunction();
+	});
+
+	$(window).resize(function () { mySetupFunction(); });
